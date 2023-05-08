26da0e929f6135a3ed75893135dbe015c8c7484c
KaustubhRakhade <77070660+KaustubhRakhade@users.noreply.github.com>
2023-05-08 20:50:09 +0530

    Add files via upload

--- CGS/gallery/app.js
+++ CGS/gallery/app.js
@@ -1,103 +1,106 @@
 /**
-
-TODO
-
+TODO:
 * zoom slider on wider screen
+* fix arrow key bug
+*/
 
- */
+DOM = (selector) => document.querySelector(selector)
 
-const gallery = document.querySelector("#gallery");
-const imgSlider = document.querySelector("#imgSlider");
-const imgViewer = document.querySelector("#imgViewer");
-const imgViewBox = document.querySelector("#imgViewBox");
-const lazyloader = document.querySelector("#lazyloader");
+const gallery = DOM("#gallery");
+const imgSlider = DOM("#imgSlider");
+const imgViewer = DOM("#imgViewer");
+const imgViewBox = DOM("#imgViewBox");
+const lazyloader = DOM("#lazyloader");
 
 var images = [];
 var i = 0;
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    images = JSON.parse(xhttp.responseText);
-    images = images.slice(0, 60);
-    console.log(images);
-    // openViewer(0)
-    populate();
-  }
-};
-xhttp.open("GET", "imagedata.json", true);
-xhttp.send();
 
-counter = 0;
-function incrementCounter() {
-  counter++;
-  console.log(counter);
-  if (counter == document.querySelectorAll("#gallery img").length) {
-    console.log("all images loaded");
-    for (i = 0; i < images.length; i++) {
-      lazyloader.innerHTML += `<img src="${images[i].download_url}">`;
-    }
-  }
-}
+fetch("imagedata.json")
+  .then(data => data.json())
+  .then((listOfImages) => {
+    images = listOfImages.slice(0, 60);
+    populate();
+})
 
 function populate() {
   for (i = 0; i < images.length; i++) {
+
+    let megapixels = ((images[i].height * images[i].width) / 1000000 ).toFixed(1)
+    let imgSize = Math.round(images[i].size / 1024)
+
     gallery.innerHTML += `
-            <div class="imgBox" title="${(
-              (images[i].height * images[i].width) /
-              1000000
-            ).toFixed(1)} MP &#x2022; ${(images[i].size / 1024).toFixed()} KB">
+            <div class="imgBox" title="${megapixels} MP &#x2022; ${imgSize} KB">
                 <img alt="Failed to load" onclick="openViewer(${i})" src="${images[i].thumbnail_url}">
                 <!-- <input class="material-symbols-outlined infoBTN" type="button" value="info" title="More info"> -->
-                <input onclick="downloadImg(${i}, this)" class="material-symbols-outlined downloadBTN" type="button" value="download" title="Download &#x2022; ${(images[i].size / 1024).toFixed()} KB">
-            </div>
-        `;
+                <input onclick="downloadImg(${i}, this)" class="material-symbols-outlined downloadBTN" type="button" value="download" title="Download &#x2022; ${imgSize} KB">
+            </div>`;
+
     imgSlider.innerHTML += `<img onclick="showImage(${i})" src="${images[i].thumbnail_url}">`;
+
   }
 
-  imgs = document.querySelectorAll("#gallery img");
-  imgs.forEach((img) =>
-    img.complete
-      ? incrementCounter()
-      : img.addEventListener("load", incrementCounter, false)
-  );
+  document.querySelectorAll("#gallery img").forEach((img) => {
+    if (img.complete) {
+      incrementCounter()
+    } else {
+      img.addEventListener("load", incrementCounter, false)
+    }
+  });
+}
+
+
+imgsLoaded = 0;
+function incrementCounter() {
+  imgsLoaded++;
+  console.log(imgsLoaded);
+  if (imgsLoaded == document.querySelectorAll("#gallery img").length) {
+    console.log("all images loaded");
+
+    images.forEach((img) => {
+      lazyloader.innerHTML += `<img src="${img.download_url}">`;
+    })
+  }
 }
 
 var pushed = false;
 function openViewer(index) {
+
+  // for back button navigation
   if (!pushed) {
     history.pushState(null, null, window.location.pathname);
     pushed = true;
   }
 
+  DOM("#imgViewer").style.display = "flex";
+  DOM("#navBTNs").style.width = "10.5rem";
+
   i = (images.length + index) % images.length;
-  document.querySelector("#imgViewer").style.display = "flex";
-  document.querySelector("#navBTNs").style.width = "10.5rem";
   showImage(i);
 }
 
 function closeViewer() {
   stopSlideshow();
-  document.querySelector("#imgViewer").style.display = "none";
-  document.querySelector("#navBTNs").style.width = "3.5rem";
+  DOM("#imgViewer").style.display = "none";
+  DOM("#navBTNs").style.width = "3.5rem";
 }
 
 var slideShow;
 function startSlideshow() {
-  document.querySelector("#stop_btn").style.display = "flex";
-  document.querySelector("#play_btn").style.display = "none";
-  if (document.querySelector("#imgViewer").style.display != "flex") {
+  DOM("#stop_btn").style.display = "flex";
+  DOM("#play_btn").style.display = "none";
+  if (DOM("#imgViewer").style.display != "flex") {
     openViewer(0);
   }
   clearInterval(slideShow);
   slideShow = setInterval(() => {
     // showImage(++i);
-    simulateClick(document.querySelector("#next_img"));
+    simulateClick(DOM("#next_img"));
   }, 2500);
 }
 
 function stopSlideshow() {
-  document.querySelector("#stop_btn").style.display = "none";
-  document.querySelector("#play_btn").style.display = "flex";
+  DOM("#stop_btn").style.display = "none";
+  DOM("#play_btn").style.display = "flex";
   clearInterval(slideShow);
 }
 
@@ -106,20 +109,17 @@ function showImage(index) {
   document.querySelectorAll("#imgSlider img").forEach((element) => {
     element.classList.remove("active");
   });
-  document.querySelector("#dl_btn").title = `Download • ${(
-    images[i].size / 1024
-  ).toFixed()} KB`;
+  
+  let imgSize = Math.round(images[i].size / 1024)
+  DOM("#dl_btn").title = `Download • ${imgSize} KB`;
   document.querySelectorAll("#imgSlider img")[i].classList.add("active");
-  sliderWidth =
-    imgSlider.getBoundingClientRect().right -
-    imgSlider.getBoundingClientRect().left;
+  sliderWidth = imgSlider.getBoundingClientRect().right - imgSlider.getBoundingClientRect().left;
 
-  imgSlider.scrollLeft =
-    -24 +
-    document.querySelectorAll("#imgSlider img")[i].offsetLeft -
-    sliderWidth / 2;
+  imgSlider.scrollLeft = -24 + document.querySelectorAll("#imgSlider img")[i].offsetLeft - sliderWidth / 2;
+  // console.log(i, imgSlider.scrollLeft)
 
   imgViewBox.style.background = `center / contain no-repeat url(${images[i].download_url})`;
+
   delX = 0;
   delY = 0;
   zoomScale = 1;
@@ -140,25 +140,20 @@ zoomScale = 1;
 
 prevTimeStamp = 0;
 
-imgViewer.addEventListener(
-  "wheel",
-  (e) => {
-    if (e.deltaY != 0) {
-      e.preventDefault();
-    }
-  },
-  { passive: false }
-);
+imgViewer.addEventListener( "wheel", (e) => {
+
+  if (e.deltaY != 0) {
+    e.preventDefault();
+  }
+
+}, { passive: false });
 
 imgViewBox.addEventListener("wheel", function (e) {
   e.preventDefault();
   var rect = imgViewBox.getBoundingClientRect();
 
   if (e.ctrlKey && e.timeStamp - prevTimeStamp > 50) {
-    if (
-      !(zoomScale == 4 && e.wheelDeltaY / 1250 > 0) &&
-      !(zoomScale == 0.25 && e.wheelDeltaY / 1250 < 0)
-    ) {
+    if (!(zoomScale == 4 && e.wheelDeltaY > 0) && !(zoomScale == 0.25 && e.wheelDeltaY < 0)) {
       zoomScale = zoomScale * (1 + e.wheelDeltaY / 1250);
       zoomScale = Math.max(0.25, Math.min(zoomScale, 4));
 
@@ -166,6 +161,7 @@ imgViewBox.addEventListener("wheel", function (e) {
         (rect.left + rect.right) / 2,
         (rect.top + rect.bottom) / 2,
       ];
+
       delX += (midpoint[0] - e.x) * (e.wheelDeltaY / 1250);
       delY += (midpoint[1] - e.y) * (e.wheelDeltaY / 1250);
       doTransform(delX, delY, zoomScale);
@@ -188,6 +184,7 @@ imgViewBox.addEventListener("dblclick", function (e) {
     delY = 0;
     zoomScale = 1;
     doTransform(delX, delY, zoomScale);
+
   } else {
     wheelDeltaY = 2;
     zoomScale *= 3;
@@ -198,50 +195,46 @@ imgViewBox.addEventListener("dblclick", function (e) {
   }
 });
 
-window.addEventListener(
-  "popstate",
-  function (event) {
-    // The popstate event is fired each time when the current history entry changes.
-    if (document.querySelector("#imgViewer").style.display == "flex") {
-      closeViewer();
-      history.pushState(null, null, window.location.pathname);
-    }
-  },
-  false
-);
+window.addEventListener("popstate", (e) => {
+  // The popstate event is fired each time when the current history entry changes.
+  if (DOM("#imgViewer").style.display == "flex") {
+    closeViewer();
+    history.pushState(null, null, window.location.pathname);
+  }
+}, false);
 
 function downloadImg(index, elem) {
   elem.value = "downloading";
   stopSlideshow();
 
-  const url = images[index].download_url;
-  const options = {
+  url = images[index].download_url;
+  options = {
     Accept: "application/json",
     "Content-Type": "application/json",
     cache: "force-cache",
   };
 
   fetch(url, options)
-    .then((response) => {
-      response.blob().then((blob) => {
-        let url = window.URL.createObjectURL(blob);
-        let a = document.createElement("a");
-        a.href = url;
-        a.download = "CGS_gallery_" + index + ".jpg";
-        a.click();
+  .then((response) => {
+    response.blob().then((blob) => {
+      let url = window.URL.createObjectURL(blob);
+      let a = document.createElement("a");
+      a.href = url;
+      a.download = "CGS_gallery_" + index + ".jpg";
+      a.click();
 
-        elem.value = "download_done";
-        setTimeout(() => {
-          elem.value = "download";
-        }, 2000);
-      });
-    })
-    .catch((rejected) => {
-      elem.value = "wifi_off";
+      elem.value = "download_done";
       setTimeout(() => {
         elem.value = "download";
-      }, 5000);
+      }, 2000);
     });
+  })
+  .catch((rejected) => {
+    elem.value = "wifi_off";
+    setTimeout(() => {
+      elem.value = "download";
+    }, 5000);
+  });
 }
 
 function simulateClick(elem) {
@@ -256,17 +249,22 @@ function simulateClick(elem) {
 document.addEventListener("keydown", function (e) {
   console.log(e);
 
-  if (document.querySelector("#imgViewer").style.display == "flex") {
-    if (e.code == "ArrowRight") {
-      // showImage(i+1)
-      simulateClick(document.querySelector("#next_img"));
-    }
-    if (e.code == "ArrowLeft") {
-      // showImage(i-1)
-      simulateClick(document.querySelector("#prev_img"));
-    }
-    if (e.code == "Escape") {
-      closeViewer();
+  if (DOM("#imgViewer").style.display == "flex") {
+    switch (e.code) {
+
+      case "ArrowRight":
+        // showImage(i+1)
+        simulateClick(DOM("#next_img"));
+        break;
+
+      case "ArrowLeft":
+        // showImage(i-1)
+        simulateClick(DOM("#prev_img"));
+        break;
+
+      case "Escape":
+        closeViewer();
+        break;
     }
   }
 });
